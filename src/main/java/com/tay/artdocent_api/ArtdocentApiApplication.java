78df589f993c4db4d2bbfd12e6bbcfc190a5a52a
a-taeyeon <taeyeon0107@naver.com>
2024-06-24 23:16:40 +0900

    Initial commit

---
+++ src/main/java/com/tay/artdocent_api/ArtdocentApiApplication.java
@@ -0,0 +1,13 @@
+package com.tay.artdocent_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArtdocentApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArtdocentApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tay/artdocent_api/ArtdocentApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tay.artdocent_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArtdocentApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
